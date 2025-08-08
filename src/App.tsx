860b4172055d41c90c52373cb90ea67fa6ba172d
ds <positivehoop@yahoo.co.jp>
2025-08-09 00:19:21 +0900

    add DarkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Title from './components/Title'
 import Form from './components/Form'
 import Results from './components/Results'
@@ -13,6 +13,17 @@ type ResultsStateType = {
   icon: string;
 }
 function App() {
+  // ダークモード切替
+  const [isDarkMode, setIsDarkMode] = useState(false);　// isDarkModeがstateで現在の状態を示すブール値、setIsDarkModeはそれを更新する関数 useEffectで状態に応じたDOM操作
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]); // isDarkModeの変更時のみ実行
+
+  //　ローディング
   const [loading, setLoading] = useState<boolean>(false);
   const [city, setCity] = useState<string>(""); // cityがstate(保管場所)setCityがstateにデータを書き込んだり操作する仕組み 他の名前でも可能だが慣例的にset+state名
   const [results, setResults] = useState<ResultsStateType>({
@@ -22,6 +33,7 @@ function App() {
     conditionText: "",
     icon: ""
   });
+  
   // ここで環境変数からAPIキーを取得
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -49,6 +61,9 @@ function App() {
     <>
       <div className="wrapper">
         <div className="container">
+          <button className="modeBtn"  onClick={() => setIsDarkMode(!isDarkMode)}> <span className='modeBtnLetter'>
+            {isDarkMode ? 'Light' : 'Dark'}</span>
+          </button>
           <Title />
           <Form setCity={setCity} getWeather={getWeather} city={city} />
           {loading? <Loading />: <Results results={results} />}
